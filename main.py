9b24cd5662179b84d261ad7aedbb67458e008445
pixeldeee <aitiiigg1@gmail.com>
2023-06-30 00:20:08 +0300

    Delete old code

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pages.history import init_history
 
 def main(page: ft.Page):
     def change(e: ft.ControlEvent):
-        page.controls = []
+        page.controls.clear()
         page.update()
 
         match e.control.selected_index:
@@ -22,6 +22,25 @@ def main(page: ft.Page):
             ft.NavigationDestination(icon=ft.icons.HISTORY, label='History')
         ]
     )
+
+    def synthetic_event(page: ft.Page, control: ft.NavigationBar):
+        control.on_change(
+            ft.ControlEvent(
+                target=control.uid,
+                name="change",
+                data=str(control.selected_index),
+                control=control,
+                page=page
+            )
+        )
+
+    page.navigation_bar.did_mount = lambda: synthetic_event(
+        page=page, control=page.navigation_bar
+    )
+
+    # call the did_mount() once manually if you mess up the order of page.update()
+    # page.navigation_bar.did_mount()
+    page.update()
 
     init_clicker(page)
     page.update()
--- pages/history.py
+++ pages/history.py
@@ -1,4 +1,10 @@
 import flet as ft
 
 def init_history(page: ft.Page):
+    page.appbar = ft.AppBar(
+        title=ft.Text('Click test'),
+        center_title=False,
+        bgcolor=ft.colors.GREEN_100,
+    )
+
     page.add(ft.Text('Test'))
